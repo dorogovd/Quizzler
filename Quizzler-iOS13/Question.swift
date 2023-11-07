25254a8c4aeea72732cb071f450d95051dc4358d
Dima <dorogovd@yandex.ru>
2023-11-07 20:06:58 +0300

    update project

--- Quizzler-iOS13/Question.swift
+++ Quizzler-iOS13/Question.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        self.text = q
+        self.answer = a
+    }
+}
